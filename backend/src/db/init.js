e02c54d75d31ed9cf707a629a8a0cc7b76cf1f86
Dmytro Torianik <dmytro.torianik@gmail.com>
2024-10-08 22:44:35 +0200

    chore: configured fucking everything

---
+++ backend/src/db/init.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+export function initDatabase() {
+  const DATABASE_URL = process.env.DATABASE_URL
+  mongoose.connection.on('open', () => {
+    console.info('Successfully connected to database:', DATABASE_URL)
+  })
+  return mongoose.connect(DATABASE_URL)
+}
